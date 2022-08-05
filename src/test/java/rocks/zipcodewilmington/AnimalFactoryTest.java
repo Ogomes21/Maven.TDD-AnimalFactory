bb6cde0ff18017318df644c631d8b4b0daf48c45
octavia <Ogomes21@gmail.com>
2022-08-05 16:46:48 -0400

    Passed all Animal Factory tests.

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,50 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
-    //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
-    //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+    //DONE - Create Test for `Animal createDog(String name, Date birthDate)`
+    //DONE - Create Test for `Animal createCat(String name, Date birthDate)`
+
+    @Test
+    public void createDogTest() {
+
+        //Given
+        String expectedName = "Benji";
+        Date expectedBirthDate = new Date();
+
+        //When
+        Dog dog = AnimalFactory.createDog(expectedName, expectedBirthDate);
+
+        //Then
+        Assert.assertEquals(expectedName, dog.getName());
+        Assert.assertEquals(expectedBirthDate, dog.getBirthDate());
+    }
+
+    @Test
+    public void createCatTest() {
+
+        //Given
+        String expectedName = "Money";
+        Date expectedBirthDate = new Date();
+
+        //When
+        Cat cat = AnimalFactory.createCat(expectedName, expectedBirthDate);
+
+        //Then
+        Assert.assertEquals(expectedName, cat.getName());
+        Assert.assertEquals(expectedBirthDate, cat.getBirthDate());
+
+    }
 }
+
+
